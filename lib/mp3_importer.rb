0b30d5db5f23535728b6a6d7a0f9d1a08722116e
Christerle Nounoute <christerlesn@gmail.com>
2020-10-13 19:14:30 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def files
+    @file_path
+  end
+
+  def import(files)
+
+  end
+
+end #end of class MP3Importer

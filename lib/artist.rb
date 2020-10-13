68d9cff4920e94f17ac576a1188fee9b2a814d8c
Christerle Nounoute <christerlesn@gmail.com>
2020-10-13 18:52:21 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.name.nil?
+      name == Artist.new(name)
+    else
+      self.name
+    end
+  end
+
+
+end #end of class Artist
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+end #end of class Song
+
+
+# # akon = Artist.new("Akon")
+# # akon.add_song(freedom)
+# freedom.artist
